0190588b82aa60302423ee88771067734e925c8c
nickcode95 <nicholasm.marie@gmail.com>
2022-11-02 18:40:30 +1100

    fix checkbox position

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+  // Set initial state of user input to empty
+  const [userText, changeUserText] =useState ('');
+  // Create an empty array to push user inputted list items to
+  const [buttonText, changeButtonText]: any[] =useState([]);
+
+// Capture value from user input and store in userText object
+ let handleChange = function  (e: any) {
+  changeUserText(e.target.value);
+   }
+
+// Pass value of userText to buttonText array
+   function handleClick () {
+    changeButtonText([userText, ...buttonText]);
+    changeUserText('');
+   }
+
+
+// Map through array of to do list items and render each item in a list item
+   const arrayItems = buttonText.map((buttonText: any, i: any)=>
+   <li key={i} id="list"> 
+    <input type="checkbox" className='checkBox'  ></input>
+   <span>{buttonText}</span>
+    </li>
+   )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={userText} name="userText"/>
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        {/* Render map in list */}
+        <ul>
+          {arrayItems}
+        </ul>
+      </div>
     </div>
   );
 }
