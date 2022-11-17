39285b5f5a6dfb050d46652c731738b621459caf
nickcode95 <nicholasm.marie@gmail.com>
2022-11-17 18:41:35 +1100

    fixed checkbox position

--- src/components/ToDoItems.tsx
+++ src/components/ToDoItems.tsx
@@ -1,25 +1,30 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 
 function ToDoItems(props: any) {
     // Create state to see if the returned div has been clicked, if it has then we apply the line through style
-    const [isClicked, setClick] =  useState(false);
+    const [isClicked, setClick] = useState(false);
     // Change state
-    function clickChange () {
+    function clickChange() {
         setClick(prevState => !prevState);
     }
     return (
-        <div className='listStyle'>
-            <img src="images/trash.png" alt="delete icon" id="trash" onClick={() => {
+        <div className='listStyle flex-container'>
+            <div className='img-flex-item'>
+                <img src="images/trash.png" alt="delete icon" id="trash" onClick={() => {
                     props.onChecked(props.id);
                 }} ></img>
-            <li key={props.i} 
-            style={{textDecoration: isClicked ? 'line-through' : 'none'}}
-            onChange={clickChange}
-                >
-                <input type="checkbox" ></input>
-                <span>{props.text}</span>
-            </li>
+            </div>
+            <div className='li-flex-item'>
+                <li key={props.i}
+                    style={{ textDecoration: isClicked ? 'line-through' : 'none' }}                  
+                >{props.text}
+                </li>
+
+            </div>
+            <div className='checkbox-flex-item'>
+                <input onClick={clickChange} type="checkbox" ></input>
+            </div>
+
         </div>
 
     )
